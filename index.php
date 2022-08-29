20bfae0bd1c9d3ec4d31837a4b4f1fdf5af2d093
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 00:49:49 +0700

    update

--- index.php
+++ index.php
@@ -1,219 +1,32 @@
 <!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Animated Login Form | CodingNepal</title>
+    <link rel="stylesheet" href="css/stylelogin.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>Login</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="pass">Forgot Password?</div>
+        <input type="submit" value="Login">
+        <div class="signup_link">
+          Not a member? <a href="regis.php">Signup</a>
+        </div>
+      </form>
+    </div>
 
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
-
-<html lang="en-us">
-
-<head>
-   <meta charset="utf-8">
-   <title>เว็บคนหล่อเท่</title>
-
-   <!-- mobile responsive meta -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-   <meta name="description" content="This is meta description">
-   <meta name="author" content="Themefisher">
-
-   <!-- plugins -->
-   <link rel="preload" href="https://fonts.gstatic.com/s/opensans/v18/mem8YaGs126MiZpBA-UFWJ0bbck.woff2" style="font-display: optional;">
-   <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:600%7cOpen&#43;Sans&amp;display=swap" media="screen">
-
-   <link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
-   <link rel="stylesheet" href="plugins/slick/slick.css">
-
-   <!-- Main Stylesheet -->
-   <link rel="stylesheet" href="css/style.css">
-
-   <!--Favicon-->
-   <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-   <link rel="icon" href="images/favicon.png" type="image/x-icon">
-</head>
-
-<body>
-<!-- navigation -->
-<header class="sticky-top bg-white border-bottom border-default">
-   <div class="container">
-
-      <nav class="navbar navbar-expand-lg navbar-white">
-         <a class="navbar-brand" href="index.html">
-            <img class="img-fluid" width="150px" src="images/logo.png" alt="LogBook">
-         </a>
-         <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navigation">
-            <i class="ti-menu"></i>
-         </button>
-
-         <div class="collapse navbar-collapse text-center" id="navigation">
-            <ul class="navbar-nav ml-auto">
-               <li class="nav-item">
-                  <a class="nav-link" href="login.php">Login</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="about.html">About</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
-               </li>
-               <li class="nav-item dropdown">
-                  <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                     aria-expanded="false">Pages <i class="ti-angle-down ml-1"></i>
-                  </a>
-                  <div class="dropdown-menu">
-                     <a class="dropdown-item" href="author.html">Author</a>
-                     <a class="dropdown-item" href="post-details-1.html">Post Details 1</a>
-                     <a class="dropdown-item" href="post-details-2.html">Post Details 2</a>
-                     <a class="dropdown-item" href="post-elements.html">Post Elements</a>
-                     <a class="dropdown-item" href="privacy-policy.html">Privacy Policy</a>
-                     <a class="dropdown-item" href="terms-conditions.html">Terms Conditions</a>
-                     <a class="dropdown-item" href="terms-conditions.html">Terms Conditions</a>
-                    
-                  </div>
-               </li>
-            </ul>
-            
-            <select class="m-2 border-0" id="select-language">
-               <option id="en" value="about/" selected>En</option>
-               <option id="fr" value="fr/about/">Fr</option>
-            </select>
-
-            <!-- search -->
-            <div class="search px-4">
-               <button id="searchOpen" class="search-btn"><i class="ti-search"></i></button>
-               <div class="search-wrapper">
-                  <form action="javascript:void(0)" class="h-100">
-                     <input class="search-box pl-4" id="search-query" name="s" type="search" placeholder="Type &amp; Hit Enter...">
-                  </form>
-                  <button id="searchClose" class="search-close"><i class="ti-close text-dark"></i></button>
-               </div>
-            </div>
-
-         </div>
-      </nav>
-   </div>
-</header>
-<!-- /navigation -->
-
-<section class="section-sm">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<div class="title-bordered mb-5 d-flex align-items-center">
-					<h1 class="h4">Hi, I Am Prawit</h1>
-					<ul class="list-inline social-icons ml-auto mr-3 d-none d-sm-block">
-						<li class="list-inline-item"><a href="#"><i class="ti-facebook"></i></a>
-						</li>
-						<li class="list-inline-item"><a href="#"><i class="ti-twitter-alt"></i></a>
-						</li>
-						<li class="list-inline-item"><a href="#"><i class="ti-linkedin"></i></a>
-						</li>
-						<li class="list-inline-item"><a href="#"><i class="ti-github"></i></a>
-						</li>
-					</ul>
-				</div>
-            <center>
-				<img src="images/sanji.png" class="img-fluid w-50 mb-4 rounded-lg" alt="author">
-
-            <div class="card-body">
-               <div class="center text-light" style="margin:auto auto">
-                   
-                   <h1 class="mt-4">Update ครั้งที่ 4</h1>
-                   <h4 class="mt-3">Prawit Keawrot</h4>
-                   
-                   <h4>ประวิทย์ แก้วโรจน์</h4>
-                   <h2>ชื่อเล่น : บอล</h2>
-               </div>
-         </center>
-				
-					<hr>
-					<h4 id="my-skills--experiences">it learns:</h4>
-               <center>
-               
-                 
-                  <div class="card" style="width: 18rem;">
-                      <ul class="list-group list-group-flush">
-                          <li class="list-group-item">Github</li>
-                          <li class="list-group-item">Pipeline</li>
-                          <li class="list-group-item">Elastic Beanstlak</li>
-                          <li class="list-group-item">Amazon RDS</li>
-                          <li class="list-group-item">Route 53</li>
-                          <li class="list-group-item">z.com domain</li>
-                          <li class="list-group-item">Certificate Manager</li>
-                          <li class="list-group-item">Lightsail</li>
-
-                      </ul>
-                  </div>
-              </div>
-          </div>
-					</ul>
-            </center>
-					
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-   <footer class="section-sm pb-0 border-top border-default">
-      <div class="container">
-         <div class="row justify-content-between">
-            <div class="col-md-3 mb-4">
-               <a class="mb-4 d-block" href="index.html">
-                  <img class="img-fluid" width="150px" src="images/logo.png" alt="LogBook">
-               </a>
-               <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-            </div>
-
-            <div class="col-lg-2 col-md-3 col-6 mb-4">
-               <h6 class="mb-4">Quick Links</h6>
-               <ul class="list-unstyled footer-list">
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="contact.html">Contact</a></li>
-                  <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                  <li><a href="terms-conditions.html">Terms Conditions</a></li>
-               </ul>
-            </div>
-
-            <div class="col-lg-2 col-md-3 col-6 mb-4">
-               <h6 class="mb-4">Social Links</h6>
-               <ul class="list-unstyled footer-list">
-                  <li><a href="#">facebook</a></li>
-                  <li><a href="#">twitter</a></li>
-                  <li><a href="#">linkedin</a></li>
-                  <li><a href="#">github</a></li>
-               </ul>
-            </div>
-
-            <div class="col-md-3 mb-4">
-               <h6 class="mb-4">Subscribe Newsletter</h6>
-               <form class="subscription" action="javascript:void(0)" method="post">
-                  <div class="position-relative">
-                     <i class="ti-email email-icon"></i>
-                     <input type="email" class="form-control" placeholder="Your Email Address">
-                  </div>
-                  <button class="btn btn-primary btn-block rounded" type="submit">Subscribe now</button>
-               </form>
-            </div>
-         </div>
-         <div class="scroll-top">
-            <a href="javascript:void(0);" id="scrollTop"><i class="ti-angle-up"></i></a>
-         </div>
-         <div class="text-center">
-            <p class="content">&copy; 2020 - Design &amp; Develop By <a href="https://themefisher.com/" target="_blank">Themefisher</a></p>
-         </div>
-      </div>
-   </footer>
-
-
-   <!-- JS Plugins -->
-   <script src="plugins/jQuery/jquery.min.js"></script>
-   <script src="plugins/bootstrap/bootstrap.min.js" async></script>
-   <script src="plugins/slick/slick.min.js"></script>
-
-   <!-- Main Script -->
-   <script src="js/script.js"></script>
-</body>
+  </body>
 </html>
