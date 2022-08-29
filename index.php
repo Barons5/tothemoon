302e808d1fc10ea7f09c03449edb7f418bee4b0e
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 01:27:02 +0700

    up

--- index.php
+++ index.php
@@ -58,22 +58,8 @@
               
               
             </ul>
-            
-            <select class="m-2 border-0" id="select-language">
-               <option id="en" value="about/" selected>En</option>
-               <option id="fr" value="fr/about/">Fr</option>
-            </select>
-
-            <!-- search -->
-            <div class="search px-4">
-               <button id="searchOpen" class="search-btn"><i class="ti-search"></i></button>
-               <div class="search-wrapper">
-                  <form action="javascript:void(0)" class="h-100">
-                     <input class="search-box pl-4" id="search-query" name="s" type="search" placeholder="Type &amp; Hit Enter...">
-                  </form>
-                  <button id="searchClose" class="search-close"><i class="ti-close text-dark"></i></button>
-               </div>
-            </div>
+           
+      
 
          </div>
       </nav>
