19c4ee1a4e3a423146187d87d5ae8c36644091f3
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 13:46:38 +0700

    update

--- register.php
+++ register.php
@@ -1,11 +1,12 @@
-<?php
-	include 'connect.php';
-?>
+
+
+
+
 
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Login</title>
+  	<title>Sign Up</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -23,12 +24,12 @@
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<div class="wrap d-md-flex">
-						<div class="img" style="background-image: url(images/sanjism.png);">
+						<div class="img" style="background-image: url(images/sm.jpg);">
 			      </div>
 						<div class="login-wrap p-4 p-md-5">
 			      	<div class="d-flex">
 			      		<div class="w-100">
-			      			<h3 class="mb-4">Login</h3>
+			      			<h3 class="mb-4">Sign Up</h3>
 			      		</div>
 								
 			      	</div>
@@ -37,34 +38,29 @@
 			      			<label class="label" for="name">Username</label>
 			      			<input type="text" class="form-control" placeholder="Username" required>
 			      		</div>
+                          <div class="form-group mb-3">
+			      			<label class="label" for="name">E-maile</label>
+			      			<input type="text" class="form-control" placeholder="E-mail" required>
+			      		</div>
 		            <div class="form-group mb-3">
 		            	<label class="label" for="password">Password</label>
 		              <input type="password" class="form-control" placeholder="Password" required>
 		            </div>
+                    <div class="form-group mb-3">
+		            	<label class="label" for="text">Tel</label>
+		              <input type="password" class="form-control" placeholder="Tel" required>
+		            </div>
 		            <div class="form-group">
 		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
-			      		</div>
-		          
-		          
-		            <div class="form-group d-md-flex">
-		            	<div class="w-50 text-left">
-			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
-										</label>
-									</div>
-									<div class="w-50 text-md-right">
-										<a href="#">Forgot Password</a>
-									</div>
 		            </div>
+		           
 		          </form>
+		          <p class="text-center">Not a member? <a  href="#signup">Sign Up</a></p>
 
-
-		          <p class="text-center">Not a member? <span><a data-toggle="tab" href="register.php">Sign Up</a></span></p>
-
-		        </div>
+						
+		        
 		      </div>
-				</div>
+				
 			</div>
 		</div>
 	</section>
@@ -77,7 +73,29 @@
 	</body>
 </html>
 
-
 <?php
-	
+	if(!empty($_POST['username']) && !empty( $_POST['password']) && !empty($_POST['email'] ) && !empty($_POST['Tel'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$email = $_POST['email'];
+		$Tel = $_POST['Tel'];
+
+		$sql= "SELECT * FROM dberp WHERE username = '$username'";
+		$result=mysqli_query($conn,$sql);
+
+		if(mysqli_num_rows($result) > 0){
+			echo "<script>";
+			echo "alert('Username already exists! Please try again later!')";
+			echo "</script>";
+	}
+	else{
+		$insert = "INSERT INTO dberp (username, password, email, Tel) VALUES ('$username', '$password', '$email', '$tel')";
+		$result = mysqli_query($conn,$insert);
+		echo "<script>";
+		echo "alert('Success')";
+		echo "</script>";
+	}
+}
 ?>
+
+
