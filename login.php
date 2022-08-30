4de57d436cc6e2452e4f194d7437dd2fa63d5d2e
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 15:47:09 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,7 @@
-<?php
-	include 'connect.php';
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Login</title>
+  	<title>Login 04</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -19,18 +15,27 @@
 	<body>
 	<section class="ftco-section">
 		<div class="container">
-			
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Login #04</h2>
+				</div>
+			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<div class="wrap d-md-flex">
-						<div class="img" style="background-image: url(images/sanjism.png);">
+						<div class="img" style="background-image: url(images/bg-1.jpg);">
 			      </div>
 						<div class="login-wrap p-4 p-md-5">
 			      	<div class="d-flex">
 			      		<div class="w-100">
-			      			<h3 class="mb-4">Login</h3>
+			      			<h3 class="mb-4">Sign In</h3>
 			      		</div>
-								
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+									</p>
+								</div>
 			      	</div>
 							<form action="#" class="signin-form">
 			      		<div class="form-group mb-3">
@@ -43,9 +48,7 @@
 		            </div>
 		            <div class="form-group">
 		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
-			      		</div>
-		          
-		          
+		            </div>
 		            <div class="form-group d-md-flex">
 		            	<div class="w-50 text-left">
 			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
@@ -58,10 +61,7 @@
 									</div>
 		            </div>
 		          </form>
-
-
-		          <p class="text-center">Not a member? <span><a data-toggle="tab" href="register.php">Sign Up</a></span></p>
-
+		          <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
 		        </div>
 		      </div>
 				</div>
@@ -77,7 +77,3 @@
 	</body>
 </html>
 
-
-<?php
-	
-?>
