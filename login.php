7a59ee800874dbdf424ce8ce819a89d1206e65b1
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 16:02:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,4 @@
-<?php
-	include 'connect.php';
-?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -55,7 +53,7 @@
 									</div>
 		            </div>
 		          </form>
-		          <p class="text-center">Not a member? <a data-toggle="tab" href="register.php">Sign Up</a></p>
+		          <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
 		        </div>
 		      </div>
 				</div>
