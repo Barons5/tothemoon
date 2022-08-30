d3d880d4ce1e1ec16ad89b836507eff8db6b79a2
Barons5 <prawitnegan2538@gmail.com>
2022-08-30 09:47:38 +0700

    ed

--- register.php
+++ register.php
@@ -26,16 +26,29 @@
 			      		<div class="w-100">
 			      			<h3 class="mb-4">Sign In</h3>
 			      		</div>
-								
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+									</p>
+								</div>
 			      	</div>
 							<form action="#" class="signin-form">
 			      		<div class="form-group mb-3">
 			      			<label class="label" for="name">Username</label>
 			      			<input type="text" class="form-control" placeholder="Username" required>
 			      		</div>
+                          <div class="form-group mb-3">
+			      			<label class="label" for="name">E-maile</label>
+			      			<input type="text" class="form-control" placeholder="E-mail" required>
+			      		</div>
 		            <div class="form-group mb-3">
 		            	<label class="label" for="password">Password</label>
 		              <input type="password" class="form-control" placeholder="Password" required>
+		            </div>
+                    <div class="form-group mb-3">
+		            	<label class="label" for="text">Tel</label>
+		              <input type="password" class="form-control" placeholder="Tel" required>
 		            </div>
 		            <div class="form-group">
 		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
@@ -52,7 +65,7 @@
 									</div>
 		            </div>
 		          </form>
-		          <p class="text-center">Not a member? <a data-toggle="tab" href="register.php">Sign Up</a></p>
+		          <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
 		        </div>
 		      </div>
 				</div>
