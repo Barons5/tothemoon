8d1ad6a9c80b600e0592dff4948e500a43ea9cb4
Barons5 <prawitnegan2538@gmail.com>
2022-08-31 11:54:41 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
    
-   $conn = mysqli_connect("db-tmm.csbcwbk7v2p0.ap-southeast-1.rds.amazonaws.com","admin","p0957965315","tb-tmm")
+   $conn = mysqli_connect("db-tmm.csbcwbk7v2p0.ap-southeast-1.rds.amazonaws.com","admin","p0957965315","dberp")
 
 
 ?>
