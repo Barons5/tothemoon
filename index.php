94083ccfdfa8bc454da872c3fbe282428b8f92a2
Barons5 <prawitnegan2538@gmail.com>
2022-09-01 09:12:12 +0700

    up

--- index.php
+++ index.php
@@ -1,180 +1,85 @@
-<!DOCTYPE html>
+<?php
+	include 'connect.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-<html lang="en-us">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style1.css">
 
-<head>
-   <meta charset="utf-8">
-   <title>เว็บคนหล่อเท่</title>
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="img" style="background-image: url(images/com.jpg);">
+			      </div>
+						<div class="login-wrap p-4 p-md-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Sign In</h3>
+			      		</div>
+								
+			      	</div>
+							<form  method="POST"  class="signin-form">
+			      		<div class="form-group mb-3">
+			      			<label class="label" for="name">Username</label>
+			      			<input type="text" name="username" class="form-control" placeholder="Username" required>
+			      		</div>
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+		              <input type="password" name="password" class="form-control" placeholder="Password" required>
+		            </div>
+		            <div class="form-group">
+		            	<button type="submit"  class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+		            </div>
 
-   <!-- mobile responsive meta -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-   <meta name="description" content="This is meta description">
-   <meta name="author" content="Themefisher">
-
-   <!-- plugins -->
-   <link rel="preload" href="https://fonts.gstatic.com/s/opensans/v18/mem8YaGs126MiZpBA-UFWJ0bbck.woff2" style="font-display: optional;">
-   <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:600%7cOpen&#43;Sans&amp;display=swap" media="screen">
-
-   <link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
-   <link rel="stylesheet" href="plugins/slick/slick.css">
-
-   <!-- Main Stylesheet -->
-   <link rel="stylesheet" href="css/style.css">
-
-   <!--Favicon-->
-   <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-   <link rel="icon" href="images/favicon.png" type="image/x-icon">
-</head>
-
-<body>
-<!-- navigation -->
-<header class="sticky-top bg-white border-bottom border-default">
-   <div class="container">
-
-      <nav class="navbar navbar-expand-lg navbar-white">
-         <a class="navbar-brand" href="index.html">
-            <img class="img-fluid" width="150px" src="images/logo.png" alt="LogBook">
-         </a>
-         <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navigation">
-            <i class="ti-menu"></i>
-         </button>
-
-         <div class="collapse navbar-collapse text-center" id="navigation">
-            <ul class="navbar-nav ml-auto">
-               <li class="nav-item">
-                  <a class="nav-link" href="login.php">Login</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="index.php">Home</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="data.php">Table</a>
-               </li>
-              
-              
-            </ul>
-           
-      
-
-         </div>
-      </nav>
-   </div>
-</header>
-<!-- /navigation -->
-
-<section class="section-sm">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				
-            <center>
-				<img src="images/sanji.png" class="img-fluid w-50 mb-4 rounded-lg" alt="author">
-
-            <div class="card-body">
-               <div class="center text-light" style="margin:auto auto">
-                   
-                   <h1 class="mt-4">Update ครั้งที่ 4</h1>
-                   <h4 class="mt-3">Prawit Keawrot</h4>
-                   
-                   <h4>ประวิทย์ แก้วโรจน์</h4>
-                   <h2>ชื่อเล่น : บอล</h2>
-               </div>
-         </center>
-				
-					<hr>
-					<h4 id="my-skills--experiences">it learns:</h4>
-               <center>
-               
-                 
-                  <div class="card" style="width: 18rem;">
-                      <ul class="list-group list-group-flush">
-                          <li class="list-group-item">Github</li>
-                          <li class="list-group-item">Pipeline</li>
-                          <li class="list-group-item">Elastic Beanstlak</li>
-                          <li class="list-group-item">Amazon RDS</li>
-                          <li class="list-group-item">Route 53</li>
-                          <li class="list-group-item">z.com domain</li>
-                          <li class="list-group-item">Certificate Manager</li>
-                          <li class="list-group-item">Lightsail</li>
-
-                      </ul>
-                  </div>
-              </div>
-          </div>
-					</ul>
-            </center>
-					
+		            
+		          </form>
+		          <p class="text-center">Not a member? <a  href="register.php">Sign Up</a></p>
+		        </div>
+		      </div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
-   <footer class="section-sm pb-0 border-top border-default">
-      <div class="container">
-         <div class="row justify-content-between">
-            <div class="col-md-3 mb-4">
-               <a class="mb-4 d-block" href="index.html">
-                  <img class="img-fluid" width="150px" src="images/logo.png" alt="LogBook">
-               </a>
-               <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-            </div>
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
 
-            <div class="col-lg-2 col-md-3 col-6 mb-4">
-               <h6 class="mb-4">Quick Links</h6>
-               <ul class="list-unstyled footer-list">
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="contact.html">Contact</a></li>
-                  <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                  <li><a href="terms-conditions.html">Terms Conditions</a></li>
-               </ul>
-            </div>
-
-            <div class="col-lg-2 col-md-3 col-6 mb-4">
-               <h6 class="mb-4">Social Links</h6>
-               <ul class="list-unstyled footer-list">
-                  <li><a href="#">facebook</a></li>
-                  <li><a href="#">twitter</a></li>
-                  <li><a href="#">linkedin</a></li>
-                  <li><a href="#">github</a></li>
-               </ul>
-            </div>
-
-            <div class="col-md-3 mb-4">
-               <h6 class="mb-4">Subscribe Newsletter</h6>
-               <form class="subscription" action="javascript:void(0)" method="post">
-                  <div class="position-relative">
-                     <i class="ti-email email-icon"></i>
-                     <input type="email" class="form-control" placeholder="Your Email Address">
-                  </div>
-                  <button class="btn btn-primary btn-block rounded" type="submit">Subscribe now</button>
-               </form>
-            </div>
-         </div>
-         <div class="scroll-top">
-            <a href="javascript:void(0);" id="scrollTop"><i class="ti-angle-up"></i></a>
-         </div>
-         <div class="text-center">
-            <p class="content">&copy; 2020 - Design &amp; Develop By <a href="https://themefisher.com/" target="_blank">Themefisher</a></p>
-         </div>
-      </div>
-   </footer>
-
-
-   <!-- JS Plugins -->
-   <script src="plugins/jQuery/jquery.min.js"></script>
-   <script src="plugins/bootstrap/bootstrap.min.js" async></script>
-   <script src="plugins/slick/slick.min.js"></script>
-
-   <!-- Main Script -->
-   <script src="js/script.js"></script>
-</body>
+	</body>
 </html>
+
+<?php
+	if(isset($_POST['username'])&& isset($_POST['password'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		
+		$sql = "SELECT * FROM dberp WHERE member_username = '$username' AND member_password = '$password'";
+		$result = mysqli_query($conn,$sql);
+		if(mysqli_num_rows($result) == 1){
+			echo "<script>window.location='index.php'</script>";
+
+		}
+		else{
+			echo "<script>window.location='login.php'</script>";
+
+		}
+	}
+?>
+
+
+
