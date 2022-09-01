fef411e8b3c66153557d1fe2db573fb9a3f55089
Barons5 <prawitnegan2538@gmail.com>
2022-09-01 08:28:47 +0700

    up

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
                   <a class="nav-link" href="index.php">Home</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="##">Table</a>
+                  <a class="nav-link" href="data.php">Table</a>
                </li>
               
               
